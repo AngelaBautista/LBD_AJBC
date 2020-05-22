7fcb4d8cf391050b60d5f87829501dfd3b06b67f
AngelaBautista <60837886+AngelaBautista@users.noreply.github.com>
2020-05-21 23:21:30 -0500

    Add files via upload

---
+++ Practica9/Practica9Query.sql
@@ -0,0 +1,16 @@
+use PuntoDeVenta
+
+create view vista_Empleados as select Empleados.vNombre, Puestos.fSueldo from Empleados left join Puestos on Empleados.iEmpleadoId=Puestos.iPuestosId
+select*from vista_Empleados
+
+create view vista_Empresas as select Productos.vNombre, Proveedores.vEmpresa from Productos left join Proveedores on Productos.iProductosId=Proveedores.iProveedorId
+select*from vista_Empresas
+
+create view vista_Marca as select vMarca from Productos cross join Proveedores
+select*from vista_Marca
+
+create view vista_ProductoP as select AVG(fPrecio) Precio from Productos group by fPrecio having AVG(fPrecio)>15.00
+select*from vista_ProductoP
+
+create view vista_PuestoSueldo as select AVG(fSueldo) Sueldo from Puestos group by fSueldo having AVG(fSueldo)<2000.00
+select*from vista_PuestoSueldo
